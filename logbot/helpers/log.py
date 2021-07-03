438dacfbac1710d66a39e5d71b0ed32c9e8073ef
Sebastiaan Jansen <whitebird@live.be>
2021-07-03 02:30:03 +0200

    change logfile name

--- logbot/helpers/log.py
+++ logbot/helpers/log.py
@@ -1,24 +1,28 @@
 import logging
 
-logging.basicConfig(filename='bot.log', level=logging.INFO)
+logging.basicConfig(filename="logbot.log", level=logging.INFO)
 
 
 def debug(msg, *args, **kwargs):
-    print(f'DEBUG   |{msg}')
+    print(f"DEBUG   |{msg}")
     logging.debug(msg, *args, **kwargs)
 
+
 def info(msg, *args, **kwargs):
-    print(f'INFO    |{msg}')
+    print(f"INFO    |{msg}")
     logging.info(msg, *args, **kwargs)
 
+
 def warn(msg, *args, **kwargs):
-    print(f'WARNING |{msg}')
+    print(f"WARNING |{msg}")
     logging.warning(msg, *args, **kwargs)
 
+
 def error(msg, *args, **kwargs):
-    print(f'ERROR   |{msg}')
+    print(f"ERROR   |{msg}")
     logging.error(msg, *args, **kwargs)
 
+
 def fatal(msg, *args, **kwargs):
-    print(f'FATAL   |{msg}')
+    print(f"FATAL   |{msg}")
     logging.fatal(msg, *args, **kwargs)
